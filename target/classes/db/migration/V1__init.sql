9b85ec6992578e616e5387b60bd1d40bc55d74a3
Landry Guiagaing <lguiagaing@gmail.com>
2026-09-23 01:44:20 +0100

    Delete target directory
    delete target directory

--- target/classes/db/migration/V1__init.sql
+++
@@ -1,9 +0,0 @@
--- Migration initiale : création de la table products
-CREATE TABLE products (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(150) NOT NULL,
-    price NUMERIC(10,2) NOT NULL,
-    quantity INTEGER NOT NULL
-);
-
-CREATE INDEX idx_products_quantity ON products(quantity);
